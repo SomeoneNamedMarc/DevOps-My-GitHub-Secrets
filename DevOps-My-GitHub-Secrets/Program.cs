942960a6fc567302f44347e4dd7b2d5e20095b5e
SomeoneNamedMarc <thatguymarc0504@gmail.com>
2025-02-18 10:10:43 +0100

    Add project files.

---
+++ DevOps-My-GitHub-Secrets/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace DevOps_My_GitHub_Secrets
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("The program is running!");
+        }
+    }
+}
